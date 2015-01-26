0667366ad33aac160e424aa0d0a3d9c3da7cf004
Kevin Smithson <ksmithson@pocketly.com>
2015-01-25 18:17:30 -0800

    added riak support, fixed a bug with 404's on riak client

---
+++ test/session.test.js
@@ -0,0 +1,53 @@
+var mockery = require('mockery');
+var sand = require('sand');
+
+describe('Sessions', function() {
+  var app;
+  before(function(done) {
+    mockery.enable({
+      warnOnUnregistered: false
+    });
+    mockery.registerSubstitute('riak-js', 'riak-mock');
+
+    var Session = require('../lib/Session');
+    app = sand()
+      .use(Session, {
+        all: {
+          riak: {
+            bucket: 'sessions'
+          }
+        }
+      })
+      .start(done);
+  });
+
+  it('should save session', function(done) {
+    var session = {
+      test: true
+    };
+
+    global.sand.session.set('1', session, done);
+  });
+
+  it ('should get session', function(done) {
+    global.sand.session.get('1', function(err, data) {
+      data.test.should.be.ok;
+      done();
+    });
+  });
+
+  it('should destroy session', function(done) {
+    global.sand.session.destroy('1');
+    global.sand.session.get('1', function(err, data) {
+      err.should.be.an.Object;
+      done();
+    });
+  });
+
+  after(function() {
+    mockery.deregisterAll();
+    mockery.disable();
+
+    app.shutdown();
+  });
+});
